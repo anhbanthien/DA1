96576fec1978b9d88b1f22ce450bfe575b0bf87e
= <=>
2022-11-13 14:29:36 +0700

    Commited done

---
+++ DuAn1/src/main/java/config/HibernatUtil.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package config;
+
+import java.util.Properties;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernatUtil {
+
+    private static final SessionFactory FACTORY;
+
+    static {
+
+        Configuration conf = new Configuration();
+        Properties properties = new Properties();
+        properties.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=FINALASS_FPOLYSHOP_FA22_SOF205_SOF2041");
+        properties.put(Environment.USER, "sa");
+        properties.put(Environment.PASS, "16102003");
+        properties.put(Environment.SHOW_SQL, "true");
+        conf.setProperties(properties);
+
+        ServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .applySettings(conf.getProperties()).build();
+        FACTORY = conf.buildSessionFactory(registry);
+    }
+
+    public static SessionFactory getFACTORY() {
+        return FACTORY;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getFACTORY());
+    }
+
+}
