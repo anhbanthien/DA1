cc885d6b1648a1aa2b333ff664698097442673d3
= <=>
2022-11-24 14:33:57 +0700

    done change password in profile

--- DuAn1/src/main/java/views/ViewBan.java
+++ DuAn1/src/main/java/views/ViewBan.java
@@ -126,13 +126,13 @@ public class ViewBan extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Số bàn", "Trạng Thái Bàn", "Số Bàn Ghép"
             }
         ));
         jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
