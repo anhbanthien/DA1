17817ed01e09cf4b99beb4781d5cd620fdf28d7e
= <=>
2022-11-29 20:15:51 +0700

    deleted

--- DuAn1/src/main/java/views/ViewBan.java
+++ DuAn1/src/main/java/views/ViewBan.java
@@ -337,7 +337,7 @@ public class ViewBan extends javax.swing.JFrame {
     }//GEN-LAST:event_tblTableMouseClicked
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        fillToTableSecond(listInvoices);
+        fillToTableSecond((ArrayList<HoaDonModel>) new HoaDonServiceImpl().getAllHoaDon());
 
     }//GEN-LAST:event_jButton1ActionPerformed
 
